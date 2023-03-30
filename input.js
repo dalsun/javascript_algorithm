f6b7ab729bc369066d8af7d55477aeed8e56a469
dalsun <51277621+dalsun@users.noreply.github.com>
2023-03-30 18:55:55 +0900

    Update input.js

--- input.js
+++ input.js
@@ -9,3 +9,20 @@ let a = parseInt(line[0]); // 1
 let b = parseInt(line[1]); // 2
 
 console.log(a + b);
+
+
+// 세자리 수 * 세자리 수
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let a = input[0];
+let b = input[1];
+
+x_1 = b[2];
+x_2 = b[1];
+x_3 = b[0];
+
+console.log(Number(a) * Number(x_1));
+console.log(Number(a) * Number(x_2));
+console.log(Number(a) * Number(x_3));
+console.log(Number(a) * Number(b));
