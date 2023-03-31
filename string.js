79263d2a0f77b7eb0f8bb7e1e3018952f6d2e784
dalsun <51277621+dalsun@users.noreply.github.com>
2023-03-31 23:22:42 +0900

    Update string.js

--- string.js
+++ string.js
@@ -66,3 +66,43 @@ let newA = a[2] + a[1] + a[0];
 let newB = b[2] + b[1] + b[0];
 
 console.log(Math.max(Number(newA), Number(newB)));
+
+// 백준 1316번
+// 그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다. 
+// 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만, aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
+// 단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
+
+/* input.txt
+3
+happy
+new
+year
+*/
+
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().split('\n');
+
+let n = Number(input[0]);
+let cnt = 0;
+
+for (let i=1; i<=n; i++) {
+  let word = input[i];
+  if (check(word)) {
+    cnt += 1;
+  }
+}
+console.log(cnt);
+
+function check(data) {
+  let setData = new Set(data[0]);
+  for (let i=0; i < data.length -1; i++) {
+    if (data[i] != data[i+1]) {
+      if (setData.has(data[i+1])) {
+        return false;
+      } else {
+        setData.add(data[i+1]);
+      }
+    }
+  }
+  return true;
+}
