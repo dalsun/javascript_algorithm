27a3ee38e24f749a011fcdfa0a0d151a69363994
dalsun <51277621+dalsun@users.noreply.github.com>
2023-04-08 16:38:31 +0900

    Update array.js

--- array.js
+++ array.js
@@ -25,3 +25,37 @@ for (let i=0; i<10; i++) {
     mySet.add(data[i]%42);
 }
 console.log(mySet.size);
+
+// 백준 18870번
+
+/* input.txt
+5
+2 4 -10 4 -9
+*/
+
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+
+function compare(a,b) {
+  return a-b;
+}
+
+// 집합(set)으로 변경해 중복 값을 없앤 뒤에 다시 배열로 변환
+let newArr = [...new Set(arr)];
+newArr.sort(compare);
+
+// 0부터 차례대로 매핑(mapping) 수행
+let myMap = new Map();
+for (let i=0; i<newArr.length; i++) {
+  myMap.set(newArr[i], i);
+}
+
+let answer = "";
+for (x of arr) {
+  answer += myMap.get(x) + " ";
+}
+
+console.log(answer);
