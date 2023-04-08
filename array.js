4b1b030d21972edce95c6b5926c016fbf3ccb090
dalsun <51277621+dalsun@users.noreply.github.com>
2023-04-08 23:45:45 +0900

    Update array.js

--- array.js
+++ array.js
@@ -59,3 +59,29 @@ for (x of arr) {
 }
 
 console.log(answer);
+
+// 백준 1427번
+// 배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
+
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().split('\n');
+
+let n = input[0];  // 수(N) 입력받기
+// 0부터 9까지 각 숫자(digit)의 출현 빈도를 담을 배열(Array) 선언
+let cnt = Array(10).fill(0);
+
+for (let x of n) { // 한 자리씨 숫자(digit)를 확인하며
+  cnt[Number(x)]++;
+}
+
+let answer = "";
+// 9부터 0까지 하나씩 숫자(digit)를 확인하며
+for (let i=9; i >= 0; i--) {
+  // 출현 빈도만큼 출력하기
+  for (let j=0; j < cnt[i]; j++) {
+    answer += i + "";
+  }
+}
+
+console.log(answer);
+
