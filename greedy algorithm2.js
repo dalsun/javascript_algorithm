1e2c844cf572ada705391ec5686b71b680dbbb14
dalsun <51277621+dalsun@users.noreply.github.com>
2023-04-13 22:17:47 +0900

    Update greedy algorithm2.js

--- greedy algorithm2.js
+++ greedy algorithm2.js
@@ -1,4 +1,4 @@
-// 백주 2839번 - 설탕 배달
+// 백준 2839번 - 설탕 배달
 
 let fs = require('fs');
 let input = fs.readFileSync('./input.txt').toString().split('\n');
@@ -20,5 +20,35 @@ while (n >= 0) { // 더 이상 반복할 수 없을 때까지 반복
 }
 
 if (!flag) {
+  console.log(-1);
+}
+
+// 백준 16953번 - A->B
+
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().split('\n');
+
+let a = Number(input[0].split(" ")[0]);
+let b = Number(input[0].split(" ")[1]);
+let flag = false;
+let cnt = 1;
+
+while (a <= b) {
+  if (a==b) {
+    flag = true;
+    break;
+  }
+  if (b % 2 == 0) {
+    b = parseInt(b/2);
+  } else if (b % 10 == 1) {
+    b = parseInt(b/10);
+  } else break;
+  cnt++;
+  console.log(b);
+}
+
+if (flag) {
+  console.log(cnt);
+} else {
   console.log(-1);
 }
